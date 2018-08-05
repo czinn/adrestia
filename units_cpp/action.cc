4b15d3ee3f9954239d6450afeb7365a09f3934c4
Takareer <fakenscam@yahoo.ca>
2018-08-05 18:00:27 -0400

    Added PEP 7-ish comments to action.h, action.cc.

--- units_cpp/action.cc
+++ units_cpp/action.cc
@@ -1,54 +1,118 @@
 #include "action.h"
 #include "colour.h"
 
-Action::Action() {}
-Action::Action(Colour colour) : type(CHOOSE_TECH), colour(colour) {}
-Action::Action(std::vector<std::string> units) : type(BUILD_UNITS), units(units) {}
 
-ActionType Action::get_type() const {
-  return type;
+//----------------------------------------------------------------------------------------------------------------------
+// CONSTRUCTORS
+//----------------------------------------------------------------------------------------------------------------------
+Action::Action() {
+    /*! \brief Action's default constructor. This, alone, initializes nothing!
+     */
 }
+
+
+Action::Action(Colour colour) : type(CHOOSE_TECH), colour(colour) {
+    /*! \brief The CHOOSE_TECH constructor.
+     *  \param colour The chosen tech.
+     */
+}
+
+
+Action::Action(std::vector<std::string> units) : type(BUILD_UNITS), units(units) {
+    /*! \brief The BUILD_UNITS constructor.
+     *  \param units A list of unit ids.
+     */
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// GETTERS AND SETTERS
+//----------------------------------------------------------------------------------------------------------------------
+ActionType Action::get_type() const {
+    /*! \brief Returns the ActionType of this action. */
+
+    return type;
+}
+
 
 Colour Action::get_colour() const {
-  return colour;
+    /*! \brief Returns the selected Colour (tech) of this CHOOSE_TECH action. Behaviour is undefined if this is not
+     *         a CHOOSE_TECH action!
+     */
+
+    return colour;
 }
+
 
 const std::vector<std::string> &Action::get_units() const {
-  return units;
+    /*! \brief Returns the units built as part of this BUILD_UNITS action. Behaviour is undefined if this is not a
+     *         BUILD_UNITS action!
+     *
+     *  \returns A vector of the unit ids of the units built as part of this action.
+     */
+
+    return units;
 }
 
+
+//----------------------------------------------------------------------------------------------------------------------
+// OTHER METHODS
+//----------------------------------------------------------------------------------------------------------------------
 void to_json(json &j, const Action &a) {
-  if (a.type == CHOOSE_TECH) {
-    j["type"] = "CHOOSE_TECH";
-    j["colour"] = a.colour;
-  } else if (a.type == BUILD_UNITS) {
-    j["type"] = "BUILD_UNITS";
-    j["units"] = a.get_units();
-  }
+    /*! \brief Converts the given Action a into JSON, which goes into j. */
+
+    if (a.type == CHOOSE_TECH) {
+        j["type"] = "CHOOSE_TECH";
+        j["colour"] = a.colour;
+    }
+    else if (a.type == BUILD_UNITS) {
+        j["type"] = "BUILD_UNITS";
+        j["units"] = a.get_units();
+    }
 }
+
 
 void from_json(const json &j, Action &a) {
-  if (j["type"] == "CHOOSE_TECH") {
-    a.type = CHOOSE_TECH;
-    a.colour = j["colour"];
-  } else if (j["type"] == "BUILD_UNITS") {
-    a.type = BUILD_UNITS;
-    for (auto it = j["units"].begin(), end = j["units"].end(); it != end; it++) {
-      a.units.push_back(*it);
+    /*! \brief Edits Action a to be the Action stored in j. */
+
+    if (j["type"] == "CHOOSE_TECH") {
+        a.type = CHOOSE_TECH;
+        a.colour = j["colour"];
     }
-  }
+    else if (j["type"] == "BUILD_UNITS") {
+        a.type = BUILD_UNITS;
+        for (auto it = j["units"].begin(), end = j["units"].end(); it != end; it++) {
+            a.units.push_back(*it);
+        }
+    }
 }
 
+
 bool operator==(const Action &a1, const Action &a2) {
-  if (a1.get_type() != a2.get_type()) return false;
-  if (a1.get_type() == CHOOSE_TECH) {
-    return a1.get_colour() == a2.get_colour();
-  } else if (a1.get_type() == BUILD_UNITS) {
-    if (a1.get_units().size() != a2.get_units().size()) return false;
-    for (int i = 0; i < a1.get_units().size(); i++) {
-      if (a1.get_units()[i] != a2.get_units()[i]) return false;
+    /*! \brief Checks whether both Actions represent the same "action." */
+
+    if (a1.get_type() != a2.get_type()) {
+        return false;
     }
-    return true;
-  }
-  return false;
+
+    if (a1.get_type() == CHOOSE_TECH) {
+        // CHOOSE_TECH Actions are equal if the colour they chose is the same.
+        return a1.get_colour() == a2.get_colour();
+    }
+    else if (a1.get_type() == BUILD_UNITS) {
+        // BUILD_UNITS Actions are equal if the units they built are equal
+        if (a1.get_units().size() != a2.get_units().size()) {
+            return false;
+        }
+
+        for (int i = 0; i < a1.get_units().size(); i++) {
+            if (a1.get_units()[i] != a2.get_units()[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
 }
--- units_cpp/action.h
+++ units_cpp/action.h
@@ -1,22 +1,33 @@
+/*! Contains the class Action, which represents player actions during a turn.
+ *  There can be multiple actions per turn!
+ */
+
+
 #pragma once
+
+
 #include "colour.h"
+
 
 enum ActionType { CHOOSE_TECH = 0, BUILD_UNITS };
 class Action {
- public:
-  Action();
-  Action(Colour colour); // Choose tech action
-  Action(std::vector<std::string> units); // Build units action
-  ActionType get_type() const;
-  Colour get_colour() const;
-  const std::vector<std::string> &get_units() const;
+    public:
+        Action();
+        Action(Colour colour); // Creates a CHOOSE_TECH action, choosing the given Colour.
+        Action(std::vector<std::string> units); // Creates a BUILD_UNITS action, building the given units.
+        ActionType get_type() const;
+        Colour get_colour() const;
+        const std::vector<std::string> &get_units() const;
 
-  friend void to_json(json &j, const Action &a);
-  friend void from_json(const json &j, Action &a);
- private:
-  ActionType type;
-  Colour colour;
-  std::vector<std::string> units;
+        friend void to_json(json &j, const Action &a);  // Converts the given Action into JSON; result goes into json j
+        friend void from_json(const json &j, Action &a);  // Clones an Action from the given JSON into Action a
+
+    private:
+        ActionType type;
+        Colour colour;
+        std::vector<std::string> units;
 };
 
+
+// Actions are equal if they are of the same type, and have chosen the same Colour/units (depending on type)
 bool operator==(const Action &a1, const Action &a2);
