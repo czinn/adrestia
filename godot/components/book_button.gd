14908c9f9b94f554ab17cc8ec340b536a4a67b6c
neynt <hyriodula@gmail.com>
2018-11-21 17:43:55 -0500

    and now books can be checked

--- godot/components/book_button.gd
+++ godot/components/book_button.gd
@@ -1,8 +1,10 @@
 extends Control
 
-onready var texture_button = $vbox/texture_button
+onready var button = $vbox/button
+onready var checkmark = $vbox/button/checkmark
 onready var label = $vbox/label
 var book = null setget set_book
+onready var checked = false setget set_checked
 
 func _ready():
   redraw()
@@ -11,7 +13,12 @@ func set_book(book_):
   book = book_
   redraw()
 
+func set_checked(checked_):
+  checked = checked_
+  redraw()
+
 func redraw():
   if book == null: return
   if label == null: return
   label.text = book.get_name()
+  checkmark.visible = checked
--- godot/scenes/game.gd
+++ godot/scenes/game.gd
@@ -8,5 +8,17 @@ func _ready():
   g.clear_children(book_grid)
   for book in g.rules.get_books().values():
     var book_button = book_button_scene.instance()
-    book_button.book = book
     book_grid.add_child(book_button)
+    book_button.book = book
+    book_button.button.connect('pressed', self, 'toggle_book', [book_button])
+
+func selected_books():
+  var result = []
+  for book_button in book_grid.get_children():
+    if book_button.checked:
+      result.append(book_button.book)
+  return result
+
+func toggle_book(book_button):
+  book_button.set_checked(not book_button.checked)
+  print(selected_books())
