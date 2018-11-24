29d89a5054b2568a9697c74af0b4af90cd84b088
Charles Zinn <charles.zinn@gmail.com>
2018-11-24 15:36:20 +0000

    Add a CFR-based strategy that makes reasonable moves in a fairly reasonable amount of time (though still needs improvement)

--- cpp/ai/cfr_strategy.h
+++ cpp/ai/cfr_strategy.h
@@ -1,17 +1,14 @@
 #pragma once
 #include <random>
-#include <set>
 #include "../game_action.h"
 #include "../game_view.h"
 #include "strategy.h"
 
-class MonteStrategy : public Strategy {
+class CfrStrategy : public Strategy {
  public:
-	MonteStrategy();
-	virtual ~MonteStrategy();
+	CfrStrategy();
+	virtual ~CfrStrategy();
 	virtual GameAction get_action(const GameView &view);
  private:
 	std::mt19937 gen;
 };
-
-std::set<int> choose(std::mt19937 &gen, int n, int k);
--- cpp/ai/main.cc
+++ cpp/ai/main.cc
@@ -5,6 +5,7 @@
 #include "../game_action.h"
 #include "../game_view.h"
 #include "strategy.h"
+#include "cfr_strategy.h"
 #include "monte_strategy.h"
 #include "random_strategy.h"
 
@@ -14,7 +15,7 @@ int main() {
 	GameRules rules("rules.json");
 	GameState game(rules, (std::vector<std::vector<std::string>>){{"conjuration", "regulation"}, {"conjuration", "regulation"}});
 	std::vector<Strategy*> strategies;
-	strategies.push_back(new MonteStrategy());
+	strategies.push_back(new CfrStrategy());
 	strategies.push_back(new RandomStrategy());
 
 	while (game.winners().size() == 0) {
