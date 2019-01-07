f8541389fc54be73852a97bbeaf4eb04a22d983a
neynt <hyriodula@gmail.com>
2019-01-07 16:16:24 -0500

    Point gdnative to new protocol function

--- cpp_gdnative/src/protocol.cc
+++ cpp_gdnative/src/protocol.cc
@@ -1,6 +1,6 @@
 #include "protocol.h"
 
-#include "../../server/protocol.h"
+#include "../../server/adrestia_networking.h"
 
 #define CLASSNAME Protocol
 
@@ -18,7 +18,7 @@ void Protocol::_register_methods() {
 
 String Protocol::make_floop_request() {
   nlohmann::json j;
-  write_floop_request(j);
+  adrestia_networking::create_floop_call(j);
   String s(j.dump().c_str());
   return s;
 }
