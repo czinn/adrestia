acca5798c9c24789ef766bf1a3861694411d6298
Takareer <munchyncrunchy@yahoo.ca>
2019-03-20 15:52:06 -0400

    NON-FUNCTIONAL COMMIT. Made godot 3.1 compile on Windows. Still doesn't work though.

--- cpp_gdnative/setup_windows.ps1
+++ cpp_gdnative/setup_windows.ps1
@@ -1,7 +1,14 @@
 git submodule update --init
 
+Push-Location godot-cpp
+    git checkout -- .
+    git clean -fx
+Pop-Location
+
 godot --gdnative-generate-json-api godot-cpp/godot_api.json
 
+(Get-Content .\godot-cpp\godot_api.json).replace("typename", "type_name") | Set-Content .\godot-cpp\godot_api.json
+
 Push-Location godot-cpp
-    scons platform=windows headers=../godot_headers generate_bindings=yes
+    scons platform=windows headers_dir=../godot_headers -j4 generate_bindings=yes
 Pop-Location
